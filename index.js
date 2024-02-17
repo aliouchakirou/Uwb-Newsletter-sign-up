91268da9e622420106c3d60f881b7147b8a336f6
Chakal-MacbookPro <macbookpro@chakal.local>
2024-02-17 02:25:25 +0100

    Complete JavaScript

---
+++ index.js
@@ -0,0 +1,25 @@
+let email = document.querySelector('#email');
+let sub = document.querySelector('#sub');
+let vaild = document.querySelector('.vail');
+let box = document.querySelector('.box');
+let card = document.querySelector('.card');
+let button = document.querySelector('#dis');
+
+sub.addEventListener('click', function() {
+    if(/[a-zA-a1-9\W]+@[a-zA-Z]+.com/.test(email.value)) {
+        box.style.visibility = "hidden";
+        card.style.display = "block";
+        card.style.visibility = "visible";
+    }
+    else{
+        email.style.backgroundColor = 'hsl(4, 100%, 67%)';
+        email.style.color = "white";
+        vaild.style.display = "block";
+       
+    }
+});
+
+button.addEventListener('click', function() {
+    card.style.visibility = "hidden";
+    box.style.visibility = "visible";
+});
